49bcf7c7542468b9c6e6fee55b1f941693ddd6ca
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-11-18 19:38:57 +0900

    impl toString, fromString

--- engine/reversi.ts
+++ engine/reversi.ts
@@ -43,6 +43,158 @@ export class Reversi {
     }
   }
 
+  public count(): Count {
+    return this._count
+  }
+
+  public evaluate(): Reversi {
+    const value: (number | null)[] = new Array<number | null>(
+      Reversi.size * Reversi.size
+    ).fill(null)
+
+    for (let i: number = 0; i < Reversi.size; i++) {
+      for (let j: number = 0; j < Reversi.size; j++) {
+        if (Reversi.isReversible(this._board, this._turn, i, j)) {
+          const r: Count = Reversi.dfs(this.put(i, j))
+          value[Reversi.pos(i, j)] =
+            r[this._turn] - r[this._turn === Disk.Dark ? Disk.Light : Disk.Dark]
+        }
+      }
+    }
+
+    return new Reversi(this._board, this.turn(), value)
+  }
+
+  public get(y: number, x: number): Disk {
+    return this._board[Reversi.pos(y, x)]
+  }
+
+  public put(y: number, x: number): Reversi {
+    if (!Reversi.isReversible(this._board, this._turn, y, x)) {
+      throw new ReversiError()
+    }
+
+    const board: Disk[] = Array.from(this._board)
+    Reversi.reverse(board, this._turn, y, x)
+
+    const turn: Disk =
+      this._turn === Disk.Dark
+        ? Reversi.isReversibleSome(board, Disk.Light)
+          ? Disk.Light
+          : Reversi.isReversibleSome(board, Disk.Dark)
+          ? Disk.Dark
+          : Disk.None
+        : Reversi.isReversibleSome(board, Disk.Dark)
+        ? Disk.Dark
+        : Reversi.isReversibleSome(board, Disk.Light)
+        ? Disk.Light
+        : Disk.None
+
+    const value: (number | null)[] = new Array<number | null>(
+      Reversi.size * Reversi.size
+    ).fill(null)
+
+    return new Reversi(board, turn, value)
+  }
+
+  public toString(): string {
+    const from: Disk[] = [Disk.None, Disk.Dark, Disk.Light]
+    const to: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split(
+      ''
+    )
+
+    let n: BigInt = this._board
+      .concat(this._turn)
+      .map(
+        (value: Disk): BigInt => {
+          // eslint-disable-next-line no-undef
+          return BigInt(from.indexOf(value))
+        }
+      )
+      .reduce((previousValue: BigInt, currentValue: BigInt): BigInt => {
+        // @ts-ignore
+        // eslint-disable-next-line no-undef
+        return BigInt(from.length) * previousValue + currentValue
+
+        // eslint-disable-next-line no-undef
+      }, BigInt(0))
+
+    const ret: string[] = []
+
+    // @ts-ignore
+    // eslint-disable-next-line no-undef
+    for (; n > BigInt(0); n /= BigInt(to.length)) {
+      // @ts-ignore
+      // eslint-disable-next-line no-undef
+      ret.push(to[n % BigInt(to.length)])
+    }
+
+    return ret.reverse().join('')
+  }
+
+  public turn(): Disk {
+    return this._turn
+  }
+
+  public value(y: number, x: number): number | null {
+    return this._value[Reversi.pos(y, x)]
+  }
+
+  public static fromString(str: string): Reversi {
+    const from: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split(
+      ''
+    )
+    const to: Disk[] = [Disk.None, Disk.Dark, Disk.Light]
+
+    let n: BigInt = str
+      .split('')
+      .map(
+        (value: string): BigInt => {
+          // eslint-disable-next-line no-undef
+          return BigInt(from.indexOf(value))
+        }
+      )
+      .reduce((previousValue: BigInt, currentValue: BigInt): BigInt => {
+        // @ts-ignore
+        // eslint-disable-next-line no-undef
+        return BigInt(from.length) * previousValue + currentValue
+
+        // eslint-disable-next-line no-undef
+      }, BigInt(0))
+
+    const ret: Disk[] = []
+
+    // @ts-ignore
+    // eslint-disable-next-line no-undef
+    for (; n > BigInt(0); n /= BigInt(to.length)) {
+      // @ts-ignore
+      // eslint-disable-next-line no-undef
+      ret.push(to[n % BigInt(to.length)])
+    }
+
+    ret.reverse()
+
+    const board: Disk[] = new Array<Disk>(this.size * this.size)
+      .fill(Disk.None)
+      .map(
+        (value: Disk, index: number): Disk => {
+          if (index < ret.length - 1) {
+            return ret[index]
+          } else {
+            return value
+          }
+        }
+      )
+
+    const turn: Disk = ret[ret.length - 1]
+
+    const value: (number | null)[] = new Array<number | null>(
+      this.size * this.size
+    ).fill(null)
+
+    return new Reversi(board, turn, value)
+  }
+
   public static initialize(): Reversi {
     const board: Disk[] = new Array<Disk>(this.size * this.size).fill(Disk.None)
     board[this.pos(this.size / 2 - 1, this.size / 2 - 1)] = Disk.Light
@@ -95,8 +247,6 @@ export class Reversi {
 
     return ret
   }
-
-  // public static fromString(str: string): Reversi {}
 
   private static isReversible(
     board: Disk[],
@@ -155,8 +305,6 @@ export class Reversi {
     return false
   }
 
-  // public toString(): string {}
-
   private static pos(y: number, x: number): number {
     return this.size * y + x
   }
@@ -193,67 +341,5 @@ export class Reversi {
 
       board[this.pos(i, j)] = turn
     }
-  }
-
-  public count(): Count {
-    return this._count
-  }
-
-  public evaluate(): Reversi {
-    const value: (number | null)[] = new Array<number | null>(
-      Reversi.size * Reversi.size
-    ).fill(null)
-
-    for (let i: number = 0; i < Reversi.size; i++) {
-      for (let j: number = 0; j < Reversi.size; j++) {
-        if (Reversi.isReversible(this._board, this._turn, i, j)) {
-          const r: Count = Reversi.dfs(this.put(i, j))
-          value[Reversi.pos(i, j)] =
-            r[this._turn] - r[this._turn === Disk.Dark ? Disk.Light : Disk.Dark]
-        }
-      }
-    }
-
-    return new Reversi(this._board, this.turn(), value)
-  }
-
-  public get(y: number, x: number): Disk {
-    return this._board[Reversi.pos(y, x)]
-  }
-
-  public put(y: number, x: number): Reversi {
-    if (!Reversi.isReversible(this._board, this._turn, y, x)) {
-      throw new ReversiError()
-    }
-
-    const board: Disk[] = Array.from(this._board)
-    Reversi.reverse(board, this._turn, y, x)
-
-    const turn: Disk =
-      this._turn === Disk.Dark
-        ? Reversi.isReversibleSome(board, Disk.Light)
-          ? Disk.Light
-          : Reversi.isReversibleSome(board, Disk.Dark)
-          ? Disk.Dark
-          : Disk.None
-        : Reversi.isReversibleSome(board, Disk.Dark)
-        ? Disk.Dark
-        : Reversi.isReversibleSome(board, Disk.Light)
-        ? Disk.Light
-        : Disk.None
-
-    const value: (number | null)[] = new Array<number | null>(
-      Reversi.size * Reversi.size
-    ).fill(null)
-
-    return new Reversi(board, turn, value)
-  }
-
-  public turn(): Disk {
-    return this._turn
-  }
-
-  public value(y: number, x: number): number | null {
-    return this._value[Reversi.pos(y, x)]
   }
 }
